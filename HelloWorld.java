c5c2424cb96ba555cc49716e1d396e5eccda3250
hankaei <32535792+hankaei@users.noreply.github.com>
2017-10-19 16:09:17 +0900

    test111

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Good Bye !!");  //test
+        System.out.println("Good Bye !!");  //test111
         System.exit(0);
         System.out.println("Hello NAIST !!");
     }
